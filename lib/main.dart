b69e6f926d233a6a623d6c0b71f09ab19fcecf63
kevin <kevinlopezs15@gmail.com>
2024-02-02 12:42:02 -0500

    FEAT: ScreenUtilInit added for responsive design in flutter Description: We use flutter_screenutil: ^5.9.0 for code with responsive design into the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:gitclic/presentation/home/home_controller.dart';
 import 'package:gitclic/presentation/routes/app_pages.dart';
@@ -22,13 +23,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false, //This is for hide debug icon in screen
-      getPages: AppPages.pages, //This is for config page in GetX
-      initialRoute: Approutes.splashPage, //This is for setup inital page
-      initialBinding: BindingsBuilder(() {
-        Get.put<HomeController>(HomeController());
-      }),
+    return ScreenUtilInit(
+      designSize: const Size(1920, 1080),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner:
+            false, //This is for hide debug icon in screen
+        getPages: AppPages.pages, //This is for config page in GetX
+        initialRoute: Approutes.splashPage, //This is for setup inital page
+        initialBinding: BindingsBuilder(() {
+          Get.put<HomeController>(HomeController());
+        }),
+      ),
     );
   }
 }
